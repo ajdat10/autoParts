02d1024a725b79a27b72b985f0448a85adb5819e
Andrew Cruz <andrewcruz@Andrews-MacBook-Air.local>
2020-11-13 19:16:48 -0500

    users/posts are working

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,24 +1,55 @@
-const { Product, Comment } = require('../db/schema')
+// const { Product, Comment } = require('../db/schema')
 
 
-const CreateComment = async (req, res) => {
-    try {
-      const comment = new Comment({ ...req.body, user_id: req.params.user_id })
-      comment.save()
-      await Product.update(
-        { _id: req.params.post_id },
-        {
-          $push: {
-            comments: comment
-          }
-        }
-      )
-      res.send(comment)
-    } catch (error) {
-      throw error
-    }
-  }
+// const CreateComment = async (req, res) => {
+//     try {
+//       const comment = new Comment({ ...req.body, user_id: req.params.user_id })
+//       comment.save()
+//       await Product.update({ _id: req.params.post_id },{ $push: {comments: comment}
+//         }
+//       )
+//       res.send(comment)
+//     } catch (error) {
+//       throw error
+//     }
+//   }
 
-  module.exports ={
-      CreateComment
-  }
+
+//   const DeleteComment = async (req, res) => {
+//     try{
+//       await Comment.deleteOne({ _id: req.params.comment_id })
+//       console.log( Comment)
+//       await Product.findByIdAndUpdate(
+//         {_id: req.params.post_id},
+//         { $pull: { comments: req.params.comment_id  } },
+//         { upsert: true, new: true },
+//         (err, updatedPost) => {
+//           if(err){console.log(err)}
+//           res.send(updatedPost)
+//         }
+//         )
+//     }catch(err){
+//       throw err
+//     }
+//   }
+
+//   const UpdateComment = async (req, res) => {
+//     try {
+//       const comment = await Comment.findByIdAndUpdate(
+//         req.params.comment_id,
+//         {
+//           ...req.body
+//         },
+//         { new: true, useFindAndModify: false },
+//         )
+//       res.send(comment)
+//     } catch (error) {
+//       throw error
+//     }
+//   }
+  
+//   module.exports ={
+//       CreateComment,
+//       DeleteComment,
+//       UpdateComment
+//   }
--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -4,11 +4,11 @@ const CommentController = require('../controllers/CommentController')
 Router.post(
   '/:post_id/user/:user_id', CommentController.CreateComment
 )
-// Router.delete(
-//   '/:post_id/remove/:comment_id', CommentController.RemoveComment
-// )
-// Router.put(
-//   '/:comment_id', CommentController.UpdateComment
-// )
+Router.delete(
+  '/:post_id/delete/:comment_id', CommentController.DeleteComment
+)
+Router.put(
+  '/:comment_id', CommentController.UpdateComment
+)
 
 module.exports = Router
