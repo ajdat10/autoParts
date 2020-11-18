021273ec74d373006abbc7b37ad4df4a440b9284
Andrew Cruz <andrewcruz@Andrews-MacBook-Air.local>
2020-11-17 20:30:07 -0500

    add changes

--- client/src/services/ApiClient.js
+++ client/src/services/ApiClient.js
@@ -1,17 +1,10 @@
 import axios from 'axios'
 
-const ApiClient = axios.create({ baseURL: 'http://localhost:3001/api' })
+const ApiClient = axios.create({ baseURL: process.env.NODE_ENV === 'production'
+? `${window.location.origin}/api`
+:  'http://localhost:3001/api' })
 
-ApiClient.interceptors.request.use(
-  async (config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  (err) => Promise.reject(err)
-)
+
 
 ApiClient.interceptors.request.use(
   async (config)=>{
