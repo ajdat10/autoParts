b182a1c7bbfd5f4e2b6170fe6cce127726a19b4d
Andrew Cruz <71989360+ajdat10@users.noreply.github.com>
2022-01-27 16:29:04 -0500

    test

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const app = require('express')()
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const logger = require('morgan')
-const connection = require('./db/connection')
-const AppRouter = require('./routes/AppRouter')
+const app = require("express")();
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const connection = require("./db/connection");
+const AppRouter = require("./routes/AppRouter");
 
-
-const PORT = process.env.PORT || 3001
-const helmet = require('helmet')
+const PORT = process.env.PORT || 3001;
+const helmet = require("helmet");
 // Initialize Middleware
-app.use(logger('dev'))
-app.use(cors())
-app.use(helmet({ contentSecurityPolicy: false }))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.use(logger("dev"));
+app.use(cors());
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get('/', (req, res) => res.send({ msg: 'Server Working' }))
-app.use('/api', AppRouter)
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-// )
+// app.get('/', (req, res) => res.send({ msg: 'Server Working' }))
+app.use("/api", AppRouter);
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+);
 
 app.listen(PORT, async () => {
   try {
-    await connection
-    console.log('Database Connected')
-    console.log(`App listening on port: ${PORT}`)
+    await connection;
+    console.log("Database Connected");
+    console.log(`App listening on port: ${PORT}`);
   } catch (error) {
-   console.log(error)
+    console.log(error);
   }
-})
+});
